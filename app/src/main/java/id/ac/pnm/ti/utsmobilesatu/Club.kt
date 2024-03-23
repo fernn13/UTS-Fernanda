52a5ca7436d4f26c4d5c6cb1c26245d71afd1289
fernn13_ <fernandaadityo13@gmail.com>
2024-03-23 13:38:50 +0700

    UTS

--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Club.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Club.kt
@@ -1,16 +1,24 @@
-package ...
+package id.ac.pnm.ti.utsmobilesatu
 
 // data class Club
 data class Club(
-    val name: String,
-    val epl: ...
-    dst...
+    val name: String = "",
+    val premierLeagueTrophies: Int,
+    val faTrophies: Int,
+    val eflTrophies: Int,
+    val championsLeagueTrophies: Int,
+    val europaLeagueTrophies: Int
 ) {
-    val totalTrophy: Int
-        get() = ...
-}
+    val totalTrophies: Int
+        get() = premierLeagueTrophies + faTrophies + eflTrophies + championsLeagueTrophies + europaLeagueTrophies
 
-// extension function recap
-fun Club.recap(): String {
-    return ...
+    // extension function recap
+    fun recap(index: Int): String {
+        return "| ${(index + 1).toString().padStart(3)} | ${name.padEnd(17)} | " +
+                "${premierLeagueTrophies.toString().padStart(14)} | " +
+                "${faTrophies.toString().padStart(3)} | " +
+                "${eflTrophies.toString().padStart(3)} | " +
+                "${championsLeagueTrophies.toString().padStart(16)} | " +
+                "${europaLeagueTrophies.toString().padStart(13)} |"
+    }
 }
--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
@@ -1,49 +1,51 @@
-package ...
+package id.ac.pnm.ti.utsmobilesatu
 
 fun main() {
-    // Inisialisasi mutableList dan tambah data List
-    val clubs = ...
-    clubs.add(...)
-    dst...
+    val clubs = mutableListOf<Club>()
+    clubs.add(Club("Liverpool", 19, 8, 9, 6, 3))
+    clubs.add(Club("Manchester United", 20, 12, 6, 3, 1))
+    clubs.add(Club("Chelsea", 6, 6, 5, 2, 2))
+    clubs.add(Club("Manchester City", 8, 8, 8, 0, 0))
+    clubs.add(Club("Arsenal", 13, 14, 2, 0, 0))
 
-    // Passing a named function ::sortByNumberTrophyTotal
-    val sortedClubs = ...
-    // Hasil sorting total trofi
+    val sortedClubs = filterAndSort(clubs, ::sortByNumberTrophyTotal)
     println("Urutan klub berdasarkan jumlah total trofi: ")
-    for (club in ...) {
-        ...
+    println("|-----|-------------------|----------------|")
+    println("| No. |       Club        |  Total Trophy  |")
+    println("|-----|-------------------|----------------|")
+    sortedClubs.forEachIndexed { index, club ->
+        println("| ${(index + 1).toString().padStart(3)} | ${club.name.padEnd(17)} | ${club.totalTrophies.toString().padStart(14)} |")
     }
+    println("|-----|-------------------|----------------|")
 
-    // Passing a named function ::filterByEuropeanTrophy
-    val filteredClubs = ...
-    // Hasil sorting total trofi
-    println("Klub yang belum pernah memenangkan UCL dan UEL:")
-    for (club in ...) {
-        ...
+
+    val filteredClubs = filterAndSort(clubs, ::filterByEuropeanTrophy)
+    println("\nKlub yang belum pernah memenangkan UCL dan UEL:")
+    filteredClubs.forEachIndexed { index, club ->
+        println("${(index + 1).toString().padStart(2)}. ${club.name.padEnd(20)}")
     }
-
-    // Panggil extension function Club recap
-    println(...)
+    println()
+    println("Rekap Perolehan Trofi Klub:")
+    println("|-----|-------------------|----------------|-----|-----|------------------|---------------|")
+    println("| No. |       Club        | Premier League | FA  | EFL | Champions League | Europa League |")
+    println("|-----|-------------------|----------------|-----|-----|------------------|---------------|")
+    clubs.forEachIndexed { index, club ->
+        println(club.recap(index))
+    }
 }
+
 
 // High order function filter and sort
 fun filterAndSort(clubs: List<Club>, options: (List<Club>) -> List<Club>): List<Club> {
-    return ...
+    return options(clubs)
 }
 
 // Fungsi sorting
 fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
-    val sortedClubs = clubs.sortedByDescending {
-        ...
-    }
-    return sortedClubs
-
+    return clubs.sortedByDescending { it.totalTrophies }
 }
 
 // Fungsi filter
 fun filterByEuropeanTrophy(clubs: List<Club>): List<Club> {
-    val filteredClubs = clubs.filter {
-        ...
-    }
-    return filteredClubs
+    return clubs.filter { it.championsLeagueTrophies == 0 && it.europaLeagueTrophies == 0 }
 }
